3b8a108187fbad91cbd7c7f7b757e3871c6dd2b6
Anwesh43 <anweshthecool0@gmail.com>
2021-05-29 19:05:17 +0530

    adding SquareCornerTransition component

---
+++ src/SquareCornerTransition.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {useStyle} from './hooks'
+import withContainer from './withContainer'
+
+interface SCTProps {
+    w : number, 
+    h : number, 
+    onClick : Function, 
+    scale : number
+}
+
+const SquareCornerTransition : React.FC<SCTProps> = (props : SCTProps) => {
+    const {parentStyle, blockStyle, squareStyle} = useStyle(props.w, props.h, props.scale)
+    return (
+        <div style = {parentStyle()}>
+            <div style = {blockStyle()}></div>
+            {[0, 1, 2, 3].map(i => (<div key = {`square_${i}`}style = {squareStyle(i)}/>))}
+        </div>
+    )
+}
+
+export default withContainer(SquareCornerTransition)

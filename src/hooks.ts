e66e37752b398a81571b277aa37a10e2ff794030
Anwesh43 <anweshthecool0@gmail.com>
2021-05-29 18:11:51 +0530

    adding useStyle hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react'
+import {useState, useEffect, CSSProperties} from 'react'
 
 const scGap : number = 0.02 
 const delay : number = 20 
@@ -36,8 +36,62 @@ export const useDimension = () => {
         }
         return () => {
             window.onresize = () => {
-                
+
             }
         }
     })
+}
+
+export const useStyle = (w : number, h : number, scale : number) => {
+    const position = 'absolute'
+    const x = w / 2
+    const y = h / 2
+    const size = Math.min(w, h) / 10
+    const background = 'indigo'
+    const sf : number = Math.sin(scale * Math.PI)
+    return {
+        parentStyle() : CSSProperties {
+            const left = `${w / 2}px`
+            const top = `${h / 2}px`
+            return {
+                position, 
+                left, 
+                top
+            }
+        },
+        blockStyle() : CSSProperties {
+            const left = `${-size / 2}px`
+            const top = `${-size / 2}px`
+            const width = `${size}px`
+            const height = `${size}px`
+            const zIndex = 5
+            return {
+                left, 
+                top, 
+                width, 
+                height, 
+                background,
+                zIndex 
+            }
+        },
+
+        squareStyle(i : number) : CSSProperties {
+            const ix : number = i % 2 
+            const iy : number = Math.floor(i / 2)
+            const left : string = `${-size / 2 - size * sf * (1 - 2 * ix)}px`
+            const top : string = `${-size / 2 - size * sf * (1 - 2 * iy)}px`
+            const width : string = `${size}px`
+            const height : string = `${size}px`
+            const opacity = 0.4 
+            return {
+                left, 
+                top, 
+                width,
+                height,
+                position, 
+                background,
+                opacity 
+            }
+        }
+    }
 }

b7ad6e1ed87b31eb1821d1369629b2361080204d
Anwesh43 <anweshthecool0@gmail.com>
2021-05-29 16:36:46 +0530

    useDimension hook

---
+++ src/hooks.ts
@@ -0,0 +1,43 @@
+import {useState, useEffect} from 'react'
+
+const scGap : number = 0.02 
+const delay : number = 20 
+
+export const useAnimatedScale = () => {
+    const [scale, setScale] = useState(0)
+    const [animated, setAnimated] = useState(false)
+    return {
+        scale, 
+        start() {
+            if (!animated) {
+                setAnimated(true)
+                const interval = setInterval(() => {
+                    setScale((prev : number) => {
+                        if (prev > 1) {
+                            setAnimated(false)
+                            clearInterval(interval)
+                            return 0
+                        }
+                        return prev + scGap 
+                    })
+                }, delay)
+            }
+        }
+    }
+}
+
+export const useDimension = () => {
+    const [w, setW] = useState(window.innerWidth)
+    const [h, setH] = useState(window.innerHeight)
+    useEffect(() => {
+        window.onresize = () => {
+            setW(window.innerWidth)
+            setH(window.innerHeight)
+        }
+        return () => {
+            window.onresize = () => {
+                
+            }
+        }
+    })
+}
